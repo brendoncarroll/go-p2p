91a4857f0b06842470a16e06207d9347813f8fc0
Brendon Carroll <7072757+brendoncarroll@users.noreply.github.com>
2020-02-08 14:02:44 -0500

    s/Matrix/AdjList

--- p2ptest/networks.go
+++ p2ptest/networks.go
@@ -4,31 +4,31 @@ import (
 	"github.com/brendoncarroll/go-p2p"
 )
 
-type Matrix [][]p2p.Addr
+type AdjList = [][]p2p.Addr
 
-func Ring(swarms []p2p.Swarm) (adjMat [][]p2p.Addr) {
-	adjMat = make([][]p2p.Addr, len(swarms))
+func Ring(swarms []p2p.Swarm) (adjList AdjList) {
+	adjList = make(AdjList, len(swarms))
 	l := len(swarms)
 	for i := 0; i < l; i++ {
-		adjMat[i] = append(adjMat[i], swarms[(i+1)%l].LocalAddrs()[0])
+		adjList[i] = append(adjList[i], swarms[(i+1)%l].LocalAddrs()[0])
 	}
-	return adjMat
+	return adjList
 }
 
-func Cluster(swarms []p2p.Swarm) (adjMat [][]p2p.Addr) {
-	adjMat = make([][]p2p.Addr, len(swarms))
+func Cluster(swarms []p2p.Swarm) (adjList AdjList) {
+	adjList = make(AdjList, len(swarms))
 	for i := range swarms {
 		for j := range swarms {
-			adjMat[i] = append(adjMat[i], swarms[j].LocalAddrs()[0])
+			adjList[i] = append(adjList[i], swarms[j].LocalAddrs()[0])
 		}
 	}
-	return adjMat
+	return adjList
 }
 
-func HubAndSpoke(swarms []p2p.Swarm) (adjMat [][]p2p.Addr) {
-	adjMat = make([][]p2p.Addr, len(swarms))
+func HubAndSpoke(swarms []p2p.Swarm) (adjList AdjList) {
+	adjList = make(AdjList, len(swarms))
 	for i := 1; i < len(swarms); i++ {
-		adjMat[0] = append(adjMat[0], swarms[i].LocalAddrs()[0])
+		adjList[0] = append(adjList[0], swarms[i].LocalAddrs()[0])
 	}
-	return adjMat
+	return adjList
 }
