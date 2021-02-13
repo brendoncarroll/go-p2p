1ded56a59ffd12761b907d5c4e2b59728ebe1550
Brendon Carroll <7072757+brendoncarroll@users.noreply.github.com>
2021-02-13 11:26:02 -0500

    adjList is now a [][]int

--- p2ptest/networks.go
+++ p2ptest/networks.go
@@ -4,13 +4,11 @@ import (
 	"github.com/brendoncarroll/go-p2p"
 )
 
-type AdjList = [][]p2p.Addr
+type AdjList = [][]int
 
-func Ring(swarms []p2p.Swarm) (adjList AdjList) {
-	adjList = make(AdjList, len(swarms))
-	l := len(swarms)
-	for i := 0; i < l; i++ {
-		adjList[i] = append(adjList[i], swarms[(i+1)%l].LocalAddrs()[0])
+func Ring(n int) (adjList AdjList) {
+	for i := 0; i < n; i++ {
+		adjList[i] = append(adjList[i], (i+1)%n)
 	}
 	return adjList
 }
@@ -19,7 +17,7 @@ func Cluster(swarms []p2p.Swarm) (adjList AdjList) {
 	adjList = make(AdjList, len(swarms))
 	for i := range swarms {
 		for j := range swarms {
-			adjList[i] = append(adjList[i], swarms[j].LocalAddrs()[0])
+			adjList[i] = append(adjList[i], j)
 		}
 	}
 	return adjList
@@ -28,39 +26,20 @@ func Cluster(swarms []p2p.Swarm) (adjList AdjList) {
 func HubAndSpoke(swarms []p2p.Swarm) (adjList AdjList) {
 	adjList = make(AdjList, len(swarms))
 	for i := 1; i < len(swarms); i++ {
-		adjList[0] = append(adjList[0], swarms[i].LocalAddrs()[0])
+		adjList[0] = append(adjList[0], i)
 	}
 	return adjList
 }
 
-func Chain(swarms []p2p.Swarm) (adjList AdjList) {
-	adjList = make(AdjList, len(swarms))
+func Chain(n int) (adjList AdjList) {
+	adjList = make(AdjList, n)
 	for i := range adjList {
 		if i-1 >= 0 {
-			adjList[i] = append(adjList[i], swarms[i-1].LocalAddrs()[0])
+			adjList[i] = append(adjList[i], i-1)
 		}
-		if i+1 < len(swarms) {
-			adjList[i] = append(adjList[i], swarms[i+1].LocalAddrs()[0])
+		if i+1 < n {
+			adjList[i] = append(adjList[i], i+1)
 		}
 	}
 	return adjList
-}
-
-func CastSlice(x interface{}) []p2p.Swarm {
-	swarms := []p2p.Swarm{}
-	switch x := x.(type) {
-	case []p2p.SecureAskSwarm:
-		for _, s := range x {
-			swarms = append(swarms, s)
-		}
-	case []p2p.SecureSwarm:
-		for _, s := range x {
-			swarms = append(swarms, s)
-		}
-	case []p2p.AskSwarm:
-		for _, s := range x {
-			swarms = append(swarms, s)
-		}
-	}
-	return swarms
 }
