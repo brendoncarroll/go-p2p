e5664d541a8034816922b54b81f8200dbfcc7739
Brendon Carroll <7072757+brendoncarroll@users.noreply.github.com>
2021-03-27 11:59:13 -0400

    add back peerswarm.AskSwarm

--- s/peerswarm/peerswarm.go
+++ s/peerswarm/peerswarm.go
@@ -10,3 +10,8 @@ type Swarm interface {
 	p2p.SecureSwarm
 	TellPeer(ctx context.Context, dst p2p.PeerID, data p2p.IOVec) error
 }
+
+type AskSwarm interface {
+	p2p.SecureAskSwarm
+	AskPeer(ctx context.Context, dst p2p.PeerID, data p2p.IOVec) ([]byte, error)
+}
