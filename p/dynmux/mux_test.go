dcabc014db2b90c112c9208d28d3f86c53c61882
Brendon Carroll <7072757+brendoncarroll@users.noreply.github.com>
2021-07-08 21:57:37 -0400

    fix dynmux

--- p/dynmux/mux_test.go
+++ p/dynmux/mux_test.go
@@ -8,6 +8,7 @@ import (
 	"github.com/brendoncarroll/go-p2p/s/memswarm"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"golang.org/x/sync/errgroup"
 )
 
 func TestMux(t *testing.T) {
@@ -19,25 +20,43 @@ func TestMux(t *testing.T) {
 	m2 := MultiplexSwarm(s2)
 
 	m1foo, err := m1.Open("foo")
-	require.Nil(t, err)
+	require.NoError(t, err)
 	m2foo, err := m2.Open("foo")
-	require.Nil(t, err)
+	require.NoError(t, err)
 	m1bar, err := m1.Open("bar")
-	require.Nil(t, err)
+	require.NoError(t, err)
 	m2bar, err := m2.Open("bar")
+	require.NoError(t, err)
 
+	eg := errgroup.Group{}
+	ctx := context.Background()
 	var recvFoo, recvBar string
-	go m1foo.ServeTells(func(msg *p2p.Message) {
-		recvFoo = string(msg.Payload)
+	eg.Go(func() error {
+		var src, dst p2p.Addr
+		buf := make([]byte, m1foo.MaxIncomingSize())
+		n, err := m1foo.Recv(ctx, &src, &dst, buf)
+		if err != nil {
+			return err
+		}
+		recvFoo = string(buf[:n])
+		return nil
 	})
-	go m1bar.ServeTells(func(msg *p2p.Message) {
-		recvBar = string(msg.Payload)
+	eg.Go(func() error {
+		var src, dst p2p.Addr
+		buf := make([]byte, m1foo.MaxIncomingSize())
+		n, err := m1bar.Recv(ctx, &src, &dst, buf)
+		if err != nil {
+			return err
+		}
+		recvBar = string(buf[:n])
+		return nil
 	})
 
 	err = m2foo.Tell(context.TODO(), m1foo.LocalAddrs()[0], p2p.IOVec{[]byte("hello foo")})
 	require.Nil(t, err)
 	err = m2bar.Tell(context.TODO(), m1bar.LocalAddrs()[0], p2p.IOVec{[]byte("hello bar")})
 	require.Nil(t, err)
+	require.NoError(t, eg.Wait())
 
 	assert.Equal(t, "hello foo", recvFoo)
 	assert.Equal(t, "hello bar", recvBar)
