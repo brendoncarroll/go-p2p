1f1a6a6e4889c53c858a15fe8802d2e58ee2e90c
Brendon Carroll <7072757+brendoncarroll@users.noreply.github.com>
2022-01-17 15:32:06 -0500

    fix p2pconn

--- p2pconn/packetconn.go
+++ p2pconn/packetconn.go
@@ -13,21 +13,21 @@ import (
 // NewPacketConn turns a Swarm into a net.PacketConn
 // Asks are not served, if you have an ask swarm you will need to handle or discard asks
 // separately
-func NewPacketConn(s p2p.Swarm) net.PacketConn {
-	return &packetConn{
+func NewPacketConn[A p2p.Addr](s p2p.Swarm[A]) net.PacketConn {
+	return &packetConn[A]{
 		swarm: s,
 	}
 }
 
-type packetConn struct {
-	swarm p2p.Swarm
+type packetConn[A p2p.Addr] struct {
+	swarm p2p.Swarm[A]
 
 	mu                          sync.Mutex
 	readDeadline, writeDeadline *time.Time
 }
 
-func (c *packetConn) WriteTo(p []byte, to net.Addr) (int, error) {
-	target := to.(Addr)
+func (c *packetConn[A]) WriteTo(p []byte, to net.Addr) (int, error) {
+	target := to.(Addr[A])
 	ctx, cf := c.getWriteContext()
 	defer cf()
 	if err := c.swarm.Tell(ctx, target.Addr, p2p.IOVec{p}); err != nil {
@@ -36,11 +36,11 @@ func (c *packetConn) WriteTo(p []byte, to net.Addr) (int, error) {
 	return len(p), nil
 }
 
-func (c *packetConn) ReadFrom(p []byte) (n int, from net.Addr, err error) {
+func (c *packetConn[A]) ReadFrom(p []byte) (n int, from net.Addr, err error) {
 	ctx, cf := c.getReadContext()
 	defer cf()
-	if err := c.swarm.Receive(ctx, func(m p2p.Message) {
-		from = Addr{Swarm: c.swarm, Addr: m.Src}
+	if err := c.swarm.Receive(ctx, func(m p2p.Message[A]) {
+		from = Addr[A]{Swarm: c.swarm, Addr: m.Src}
 		n = copy(p, m.Payload)
 	}); err != nil {
 		return 0, nil, err
@@ -48,14 +48,14 @@ func (c *packetConn) ReadFrom(p []byte) (n int, from net.Addr, err error) {
 	return n, from, nil
 }
 
-func (c *packetConn) LocalAddr() net.Addr {
-	return Addr{
+func (c *packetConn[A]) LocalAddr() net.Addr {
+	return Addr[A]{
 		Addr:  c.swarm.LocalAddrs()[0],
 		Swarm: c.swarm,
 	}
 }
 
-func (c *packetConn) SetDeadline(t time.Time) error {
+func (c *packetConn[A]) SetDeadline(t time.Time) error {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 	c.readDeadline = &t
@@ -63,21 +63,21 @@ func (c *packetConn) SetDeadline(t time.Time) error {
 	return nil
 }
 
-func (c *packetConn) SetReadDeadline(t time.Time) error {
+func (c *packetConn[A]) SetReadDeadline(t time.Time) error {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 	c.readDeadline = &t
 	return nil
 }
 
-func (c *packetConn) SetWriteDeadline(t time.Time) error {
+func (c *packetConn[A]) SetWriteDeadline(t time.Time) error {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 	c.writeDeadline = &t
 	return nil
 }
 
-func (c *packetConn) getReadContext() (context.Context, context.CancelFunc) {
+func (c *packetConn[A]) getReadContext() (context.Context, context.CancelFunc) {
 	ctx := context.Background()
 	c.mu.Lock()
 	defer c.mu.Unlock()
@@ -87,7 +87,7 @@ func (c *packetConn) getReadContext() (context.Context, context.CancelFunc) {
 	return context.WithCancel(ctx)
 }
 
-func (c *packetConn) getWriteContext() (context.Context, context.CancelFunc) {
+func (c *packetConn[A]) getWriteContext() (context.Context, context.CancelFunc) {
 	ctx := context.Background()
 	c.mu.Lock()
 	defer c.mu.Unlock()
@@ -97,24 +97,24 @@ func (c *packetConn) getWriteContext() (context.Context, context.CancelFunc) {
 	return context.WithCancel(ctx)
 }
 
-func (c *packetConn) Close() error {
+func (c *packetConn[A]) Close() error {
 	return c.swarm.Close()
 }
 
-type Addr struct {
-	p2p.Swarm
-	p2p.Addr
+type Addr[A p2p.Addr] struct {
+	p2p.Swarm[A]
+	Addr A
 }
 
-func NewAddr(s p2p.Swarm, a p2p.Addr) net.Addr {
-	return Addr{s, a}
+func NewAddr[A p2p.Addr](s p2p.Swarm[A], a A) net.Addr {
+	return Addr[A]{s, a}
 }
 
-func (a Addr) Network() string {
+func (a Addr[A]) Network() string {
 	return fmt.Sprintf("p2p-%T-%v", a.Swarm, a.Swarm)
 }
 
-func (a Addr) String() string {
+func (a Addr[A]) String() string {
 	data, _ := a.Addr.MarshalText()
 	return string(data)
 }
