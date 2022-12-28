687fd40cb07901adb08f899cbd6687c62cee1403
Brendon Carroll <7072757+brendoncarroll@users.noreply.github.com>
2022-12-27 20:36:30 -0500

    x509: add Registry, Codec, Signer, and Verifier types (#30)

---
+++ f/x509/registry.go
@@ -0,0 +1,128 @@
+package x509
+
+import (
+	"fmt"
+
+	"github.com/brendoncarroll/go-p2p/crypto/sign"
+	"github.com/brendoncarroll/go-p2p/crypto/sign/sig_ed25519"
+	"github.com/brendoncarroll/go-p2p/f/x509/oids"
+)
+
+type ErrUnrecognizedAlgo struct {
+	AlgorithmID oids.OID
+}
+
+func (e ErrUnrecognizedAlgo) Error() string {
+	return fmt.Sprintf("unrecognized algorithm: %v", e.AlgorithmID)
+}
+
+// A Codec contains functions for Parsing and Marshaling Verifiers and Signers
+type Codec struct {
+	ParsePublic   func([]byte) (Verifier, error)
+	MarshalPublic func(out []byte, v Verifier) []byte
+
+	ParsePrivate   func([]byte) (Signer, error)
+	MarshalPrivate func(out []byte, s Signer) []byte
+}
+
+// NewCodec returns a Codec for a signing scheme.
+func NewCodec[Private, Public any](sch sign.Scheme[Private, Public]) Codec {
+	return Codec{
+		ParsePublic: func(x []byte) (Verifier, error) {
+			pub, err := sch.ParsePublic(x)
+			if err != nil {
+				return nil, err
+			}
+			return NewVerifier[Private, Public](sch, &pub), nil
+		},
+		MarshalPublic: func(out []byte, v Verifier) []byte {
+			v2 := v.(*verifier[Private, Public])
+			return sign.AppendPublicKey[Public](out, sch, &v2.public)
+		},
+		ParsePrivate: func(x []byte) (Signer, error) {
+			priv, err := sch.ParsePrivate(x)
+			if err != nil {
+				return nil, err
+			}
+			return NewSigner(sch, &priv), nil
+		},
+		MarshalPrivate: func(out []byte, s Signer) []byte {
+			panic("not implemented")
+		},
+	}
+}
+
+// Registry mananges parsing Verifiers and Signers for a given set of algorithms.
+type Registry map[oids.OID]Codec
+
+func (r Registry) LoadVerifier(pk *PublicKey) (Verifier, error) {
+	codec, err := r.getCodec(pk.Algorithm)
+	if err != nil {
+		return nil, err
+	}
+	return codec.ParsePublic(pk.Data)
+}
+
+func (r Registry) StoreVerifier(algoID oids.OID, v Verifier) (PublicKey, error) {
+	codec, err := r.getCodec(algoID)
+	if err != nil {
+		return PublicKey{}, err
+	}
+	return PublicKey{
+		Algorithm: algoID,
+		Data:      codec.MarshalPublic(nil, v),
+	}, nil
+}
+
+func (r Registry) LoadSigner(pk *PrivateKey) (Signer, error) {
+	codec, err := r.getCodec(pk.Algorithm)
+	if err != nil {
+		return nil, err
+	}
+	return codec.ParsePrivate(pk.Data)
+}
+
+func (r Registry) StoreSigner(algoID oids.OID, s Signer) (PrivateKey, error) {
+	codec, err := r.getCodec(algoID)
+	if err != nil {
+		return PrivateKey{}, err
+	}
+	return PrivateKey{
+		Algorithm: algoID,
+		Data:      codec.MarshalPrivate(nil, s),
+	}, nil
+}
+
+func (r Registry) ParseVerifier(data []byte) (Verifier, error) {
+	pk, err := ParsePublicKey(data)
+	if err != nil {
+		return nil, err
+	}
+	return r.LoadVerifier(&pk)
+}
+
+func (r Registry) getCodec(algoID oids.OID) (Codec, error) {
+	codec, exists := r[algoID]
+	if !exists {
+		return Codec{}, ErrUnrecognizedAlgo{algoID}
+	}
+	return codec, nil
+}
+
+func (r Registry) PublicFromPrivate(private *PrivateKey) (ret PublicKey, _ error) {
+	s, err := r.LoadSigner(private)
+	if err != nil {
+		return ret, err
+	}
+	pubKey, err := r.StoreVerifier(private.Algorithm, s.Verifier())
+	if err != nil {
+		return ret, err
+	}
+	return pubKey, err
+}
+
+func DefaultRegistry() Registry {
+	return Registry{
+		Algo_Ed25519: NewCodec[sig_ed25519.PrivateKey, sig_ed25519.PublicKey](sig_ed25519.New()),
+	}
+}
--- f/x509/x509.go
+++ f/x509/x509.go
@@ -2,26 +2,31 @@
 package x509
 
 import (
+	"crypto"
+	"crypto/ed25519"
 	"crypto/x509/pkix"
 	"encoding/asn1"
 	"errors"
 
+	"github.com/brendoncarroll/go-p2p/crypto/sign"
+	"github.com/brendoncarroll/go-p2p/crypto/sign/sig_ed25519"
 	"github.com/brendoncarroll/go-p2p/f/x509/oids"
 )
 
 // PublicKey is an AlgorithmID and a marshaled public key
 type PublicKey struct {
-	AlgorithmID oids.OID
-	Data        []byte
+	Algorithm oids.OID
+	Data      []byte
 }
 
-func MarshalPublicKey(out []byte, x PublicKey) []byte {
+// MarshalPublicKey appends the marshalled bytes of x to out and returns the result.
+func MarshalPublicKey(out []byte, x *PublicKey) []byte {
 	data, err := asn1.Marshal(struct {
 		pkix.AlgorithmIdentifier
 		asn1.BitString
 	}{
 		pkix.AlgorithmIdentifier{
-			Algorithm: x.AlgorithmID.ASN1(),
+			Algorithm: x.Algorithm.ASN1(),
 		},
 		asn1.BitString{
 			Bytes:     x.Data,
@@ -34,6 +39,7 @@ func MarshalPublicKey(out []byte, x PublicKey) []byte {
 	return append(out, data...)
 }
 
+// ParsePublicKey attempts to parse a PublicKey from input, and returns the PublicKey or an error.
 func ParsePublicKey(input []byte) (PublicKey, error) {
 	var record struct {
 		Raw       asn1.RawContent
@@ -47,7 +53,141 @@ func ParsePublicKey(input []byte) (PublicKey, error) {
 		return PublicKey{}, errors.New("data after public key")
 	}
 	return PublicKey{
-		AlgorithmID: oids.New(record.Algorithm.Algorithm...),
-		Data:        record.PublicKey.RightAlign(),
+		Algorithm: oids.New(record.Algorithm.Algorithm...),
+		Data:      record.PublicKey.RightAlign(),
 	}, nil
+}
+
+type PrivateKey struct {
+	Algorithm oids.OID
+	Data      []byte
+}
+
+// MarshalPrivateKey appends the marshalled bytes of x to out and returns the result.
+func MarshalPrivateKey(out []byte, x *PrivateKey) []byte {
+	data, err := asn1.Marshal(struct {
+		pkix.AlgorithmIdentifier
+		asn1.BitString
+	}{
+		pkix.AlgorithmIdentifier{
+			Algorithm: x.Algorithm.ASN1(),
+		},
+		asn1.BitString{
+			Bytes:     x.Data,
+			BitLength: len(x.Data) * 8,
+		},
+	})
+	if err != nil {
+		panic(err)
+	}
+	return append(out, data...)
+}
+
+// ParsePrivateKey attempts to parse a PrivateKey from input, and returns the PrivateKey or an error.
+func ParsePrivateKey(input []byte) (PrivateKey, error) {
+	var record struct {
+		Raw       asn1.RawContent
+		Algorithm pkix.AlgorithmIdentifier
+		PublicKey asn1.BitString
+	}
+	rest, err := asn1.Unmarshal(input, &record)
+	if err != nil {
+		return PrivateKey{}, err
+	} else if len(rest) > 0 {
+		return PrivateKey{}, errors.New("data after public key")
+	}
+	return PrivateKey{
+		Algorithm: oids.New(record.Algorithm.Algorithm...),
+		Data:      record.PublicKey.RightAlign(),
+	}, nil
+}
+
+// Verifier contains the verify method
+type Verifier interface {
+	Verify(msg, sig []byte) bool
+}
+
+type verifier[Private, Public any] struct {
+	scheme sign.Scheme[Private, Public]
+	public Public
+}
+
+func (v *verifier[Private, Public]) Verify(msg, sig []byte) bool {
+	return v.scheme.Verify(&v.public, msg, sig)
+}
+
+// NewVerifier creates a Verifier from a sign.Scheme and a public key
+func NewVerifier[Private, Public any](sch sign.Scheme[Private, Public], pub *Public) Verifier {
+	return &verifier[Private, Public]{
+		scheme: sch,
+		public: *pub,
+	}
+}
+
+// Signer contains the Sign method
+type Signer interface {
+	// Sign appends a signature for msg to out, and returns out, or an error
+	Sign(out []byte, msg []byte) ([]byte, error)
+	Verifier() Verifier
+}
+
+type signer[Private, Public any] struct {
+	scheme  sign.Scheme[Private, Public]
+	private Private
+}
+
+func (s *signer[Private, Public]) Sign(out []byte, msg []byte) ([]byte, error) {
+	initLen := len(out)
+	out = append(out, make([]byte, s.scheme.SignatureSize())...)
+	s.scheme.Sign(out[:initLen], &s.private, msg)
+	return out, nil
+}
+
+func (s *signer[Private, Public]) Verifier() Verifier {
+	public := s.scheme.DerivePublic(&s.private)
+	return NewVerifier(s.scheme, &public)
+}
+
+// NewSigner returns a Signer from a sign.Scheme and a private key
+func NewSigner[Private, Public any](sch sign.Scheme[Private, Public], priv *Private) Signer {
+	return &signer[Private, Public]{
+		scheme:  sch,
+		private: *priv,
+	}
+}
+
+// SignerFromStandard returns a Signer and oids.OID for a standard library crypto.Signer
+// If the algorithm is not supported it returns the zero value for both
+func SignerFromStandard(x crypto.Signer) (oids.OID, Signer) {
+	switch x := x.(type) {
+	case ed25519.PrivateKey:
+		y := sig_ed25519.PrivateKeyFromStandard(x)
+		return Algo_Ed25519, NewSigner[sig_ed25519.PrivateKey, sig_ed25519.PublicKey](sig_ed25519.New(), &y)
+	default:
+		return "", nil
+	}
+}
+
+// VerifierFromStandard returns a Verifier and oids.OID for a standard library crypto.PublicKey
+// If the algorithm is not supported it returns the zero value for both
+func VerifierFromStandard(x crypto.PublicKey) (oids.OID, Verifier) {
+	switch x := x.(type) {
+	case ed25519.PublicKey:
+		y := sig_ed25519.PublicKeyFromStandard(x)
+		return Algo_Ed25519, NewVerifier[sig_ed25519.PrivateKey, sig_ed25519.PublicKey](sig_ed25519.New(), &y)
+	default:
+		return "", nil
+	}
+}
+
+func ToStandardSigner(x *PrivateKey) (crypto.Signer, error) {
+	switch x.Algorithm {
+	case Algo_Ed25519:
+		if len(x.Data) != ed25519.SeedSize {
+			return nil, errors.New("not a valid ed25519 private key")
+		}
+		return ed25519.NewKeyFromSeed(x.Data), nil
+	default:
+		return nil, ErrUnrecognizedAlgo{x.Algorithm}
+	}
 }
